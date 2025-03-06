2d4a520a8db19bce1ee8907208fd6f5d0bc10a0d
27-Batman <anuragsing092@gmail.com>
2025-03-06 16:06:36 +0545

    Join two variable

---
+++ Basics/Basic Dart Program/join.dart
@@ -0,0 +1,11 @@
+void main(){
+  
+  var firstName = "Bruce";
+  var lastName = "Wayne";
+  print("Hello, " + firstName + " " + lastName);
+  print("Hello, $firstName $lastName");
+
+}
+
+// This is how you can add two variables in Dart.
+// You can use the + operator or you can use the $ sign to add variables in a string.
