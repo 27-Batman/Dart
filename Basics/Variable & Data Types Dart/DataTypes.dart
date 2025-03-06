eecd0caf0700d5e701c4bfb7d08809c08447aa7f
27-Batman <anuragsing092@gmail.com>
2025-03-06 22:23:05 +0545

    Built in datatypes explain and Number example

---
+++ Basics/Variable & Data Types Dart/DataTypes.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  //Numbers
+  int age = 22; // int for integer values
+  double height = 5.8; // double for decimal values
+  num number = 10; // num can store both int and double values
+  const pi = 3.14; // const is used to declare constant values
+
+  print("Age: $age");
+  print("Height: $height");
+  print("Number: $number");
+  print("PI: $pi");
+
+  double price = 1130.2232323233233; // tosringAsFixed() is used to format the decimal values
+  print(price.toStringAsFixed(2));
+
+  
+}
