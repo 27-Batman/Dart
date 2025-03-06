ff8ad6bba4c75f697ae65e0cdda22cd4218a4668
27-Batman <anuragsing092@gmail.com>
2025-03-06 21:22:42 +0545

    Variable

---
+++ Basics/Variable & Data Types Dart/var.dart
@@ -0,0 +1,19 @@
+
+void main() {
+    
+    String name = "Bruce"; //String for text values
+    int age = 22; // int for integer values
+    double height = 5.8; // double for decimal values
+    bool isStudent = true; // bool for boolean values
+
+    num number = 10; // num can store both int and double values
+    const pi = 3.14; // const is used to declare constant values
+
+    print("Name: $name");
+    print("Age: $age");
+    print("Height: $height");
+    print("Is Student: $isStudent");
+    print("Number: $number");
+    print("PI: $pi");
+
+}
