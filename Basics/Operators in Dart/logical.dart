6396d531aec264724997a44712559f68e684982b
27-Batman <anuragsing092@gmail.com>
2025-03-23 22:54:45 +0545

    Logical Operators

---
+++ Basics/Operators in Dart/logical.dart
@@ -0,0 +1,11 @@
+void main(){
+  int userid = 169;
+    int userpin = 569;
+
+    print((userid == 169) && (userpin== 569)); 
+    print((userid == 1213) && (userpin== 569)); 
+    print((userid == 169) || (userpin== 569)); 
+    print((userid == 1213) || (userpin== 569)); 
+    print((userid == 169) != (userpin== 569));
+
+}
