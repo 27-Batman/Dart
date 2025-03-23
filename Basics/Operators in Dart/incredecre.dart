23faab7f86f6b1eab80fac413e5604c45875f2c4
27-Batman <anuragsing092@gmail.com>
2025-03-23 21:55:36 +0545

    Increment and Decrement Operators

---
+++ Basics/Operators in Dart/incredecre.dart
@@ -0,0 +1,13 @@
+void main(){
+  var num1 = 0;
+  var num2 = 2;
+
+  num2 = ++num1;
+  print("The value of num2 is $num2");
+
+  num1 = 0;
+  num2 =0;
+
+  num2 = num1++;
+  print("the value of num2 is $num2");
+}
