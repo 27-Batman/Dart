3403b6e6ad92323a5c14b5bd56ed35c99072fb14
27-Batman <anuragsing092@gmail.com>
2025-03-26 13:02:19 +0545

    Integer Input

--- 1 Introduction and Basics/1.4 User Input/user.dart
+++ 1 Introduction and Basics/1.4 User Input/user.dart
@@ -6,4 +6,9 @@ void main() {
   print("Enter name:");
   String? name  = stdin.readLineSync();
   print("The entered name is ${name}");
+
+  //Integer User Input
+  print("Enter number");
+  int? number = int.parse(stdin.readLineSync()!);
+  print("The entered number is ${number}");
 }
